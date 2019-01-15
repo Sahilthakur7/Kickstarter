662f36204e1f75f4674ce31ca521547ee65eea50
SahilThakur7 <sahilrd07@gmail.com>
2019-01-15 09:55:34 +0530

    started writing the campaign contract

---
+++ contracts/campaign.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.17;
+
+contract Campaign{
+    struct Request {
+        string description;
+        uint value;
+        address recepient;
+        bool complete;
+    }
+
+    address public manager;
+    uint public minimumContribution;
+    address[] public approvers;
+    Request[] public requests;
+
+    modifier restricted() {
+        require (msg.sender === manager);
+        _;
+    }
+
+    function Campaign(uint minimum) public {
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+
+        approvers.push(msg.sender);
+    }
+}
