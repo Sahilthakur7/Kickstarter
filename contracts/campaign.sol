d3226d586fcdcb47082128349f2bbf4f079ad97c
SahilThakur7 <sahilrd07@gmail.com>
2019-01-18 09:48:04 +0530

    contract completed

--- contracts/campaign.sol
+++ contracts/campaign.sol
@@ -14,6 +14,7 @@ contract Campaign{
     uint public minimumContribution;
     mapping(address => bool) public approvers;
     Request[] public requests;
+    uint public approversCount;
 
     modifier restricted() {
         require (msg.sender === manager);
@@ -29,6 +30,7 @@ contract Campaign{
         require(msg.value > minimumContribution);
 
         approvers[msg.sender] = true;
+        approversCount++;
     }
 
     function createRequest(string description, uint value, address recipient) public restricted {
@@ -36,9 +38,30 @@ contract Campaign{
             description: description,
             value: value,
             recipient: recipient,
-            complete: false
+            complete: false,
+            approvalCount: 0,
         });
 
         requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+    
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
     }
 }
